3af6a5d501d13598921711f3b5a9fdd751a1dbc0
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2013-12-04 14:33:10 +0900

    add experiment

---
+++ Assets/Extra/TargetCount.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetCount : MonoBehaviour
+{
+	public int count;
+	GameObject[] objects;
+
+	public float interval = 3;
+
+	float nextTime;
+
+	void Update()
+	{
+		if (Time.time > nextTime)
+		{
+			objects = GameObject.FindGameObjectsWithTag("Target");
+			count = objects.Length;
+			nextTime += interval;
+		}
+	}
+}
