2ba35195fa51639f4e08aa586748571f10df64fa
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2013-12-05 18:21:23 +0900

    switch scene add

---
+++ Assets/Extra/SwitchScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScene : MonoBehaviour
+{
+	public string[] sceneList;
+	void OnGUI()
+	{
+		foreach (string sceneName in sceneList)
+		{
+			if (GUILayout.Button(sceneName))
+			{
+				Application.LoadLevel(sceneName);
+			}
+		}
+	}
+}
