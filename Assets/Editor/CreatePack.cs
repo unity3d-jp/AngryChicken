e75d70bc3aef5303920ee561ac956448080aa3f8
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-02-26 18:03:44 +0900

    packageを生成する機能を追加（テスト用）

---
+++ Assets/Editor/CreatePack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CreatePack
+{
+	static readonly string[] packagePath =
+	{
+		"Effect",
+		"Slingshot",
+	};
+
+	[MenuItem("Export/ExportPackage")]
+	static void ExportPackage()
+	{
+		foreach (var path in packagePath)
+		{
+			string inFilePath = "Assets/" + path;
+			string exportFilePath = "Assets/" + path + ".unitypackage";
+
+			AssetDatabase.ExportPackage(inFilePath, exportFilePath, ExportPackageOptions.Recurse);
+		}
+
+		AssetDatabase.Refresh(ImportAssetOptions.Default);
+	}
+}
